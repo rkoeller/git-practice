46ab18c53b902063150766b1db301fb6d5baac5f
Ryan Koeller <koeller.8@wright.edu>
2018-08-02 16:26:00 -0400

    comment fixing

--- src/fun/Row.java
+++ src/fun/Row.java
@@ -1,3 +1,7 @@
+/**
+ * Row.java
+ * @author Ryan Koeller
+ */
 package fun;
 
 import java.lang.StringBuffer;
@@ -9,48 +13,51 @@ public class Row
 
 	public Row()
 	{
-		
+
 	}
-	
+
 	/**
 	 * Initializes nums with 1s and 0s
-     * @param randomGenerator Random object used to create random numbers
-     */
+	 * 
+	 * @param randomGenerator Random object used to create random numbers
+	 */
 	public void randomize(Random randomGenerator)
 	{
-		for(int i = 0; i < nums.length; i++)
+		for (int i = 0; i < nums.length; i++)
 		{
 			int randomNum = randomGenerator.nextInt(10);
-			if(randomNum > 5) // 40% chance
+			if (randomNum > 5) // 40% chance
 			{
 				nums[i] = 1;
-			}
-			else
+			} else
 			{
 				nums[i] = 0;
 			}
 		}
 	}
-	
+
 	/**
 	 * Adds two rows together
-	 * @return Row sum of both rows
-     */
+	 * 
+	 * @param r1 First row to be added
+	 * @param r2 Second row to be added
+	 * @return sum of both rows
+	 */
 	public static Row addTo(Row r1, Row r2)
 	{
 		Row r3 = new Row();
-		
-		for(int i = 0; i < r1.nums.length; i++)
+
+		for (int i = 0; i < r1.nums.length; i++)
 		{
 			r3.nums[i] = (byte) (r1.nums[i] + r2.nums[i]);
 		}
-		
+
 		return r3;
 	}
-	
+
 	/**
 	 * Prints nums to console
-     */
+	 */
 	public void print()
 	{
 		for (int i = 0; i < nums.length - 1; i++)
@@ -62,7 +69,7 @@ public class Row
 
 	/**
 	 * @return nums represented as a string
-     */
+	 */
 	public String toString()
 	{
 		StringBuffer newString = new StringBuffer(nums.length);
