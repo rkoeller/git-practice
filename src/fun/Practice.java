65faec7da03d9119c7d677fb96a1266a06758a5f
Ryan Koeller <koeller.8@wright.edu>
2018-08-02 06:08:04 -0400

    changed bracket style to newline style and changed line ending to LF from CRLF

--- src/fun/Practice.java
+++ src/fun/Practice.java
@@ -1,7 +1,9 @@
 package fun;
 
-public class Practice {
-    public static void main(String[] args) {
-    	System.out.println("Hello World!!");
-    }
+public class Practice
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World!!");
+	}
 }
