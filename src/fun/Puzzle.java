4746ba25bf600e04f9ef692cfeedf39a50507dd4
Ryan Koeller <koeller.8@wright.edu>
2018-08-02 16:20:39 -0400

    white space fix

--- src/fun/Puzzle.java
+++ src/fun/Puzzle.java
@@ -1,12 +1,22 @@
 package fun;
-import java.util.Random;
 
+import java.util.Random;
 
 public class Puzzle
 {
+	/**
+     * @param args the command line arguments
+     */
 	public static void main(String[] args)
 	{
 		Random randomGenerator = new Random();
-		
+		Row row1 = new Row();
+		Row row2 = new Row();
+		row1.randomize(randomGenerator);
+		row2.randomize(randomGenerator);
+		System.out.println(row1);
+		System.out.println(row2);
+		Row row3 = Row.addTo(row1, row2);
+		System.out.println(row3);
 	}
 }
